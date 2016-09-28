67da4ced3157d66196fa51eb210d5dfa76b5aa72
apmarshall <apmarshall@soren.tech>
2016-09-28 11:16:31 -0400

    Fix: Split search form into separate row from logo

--- 404.php
+++ 404.php
@@ -1,12 +1,14 @@
 <div class="container-fluid 404-container" style="background-image: url('https://media.giphy.com/media/fRZn2vraBGiA0/giphy.gif'); background-size: cover; background-position: center center;" >
      <div class="row 404-logo-row">
      	  <div class="col-xs-10 col-xs-offset-1 404-logo">
-	       <img src="https://s3.amazonaws.com/SorenTech/media/404.png" class="404-logo" />
-	       <p style="text-align: center" class="search-form">
-	       	  <?php get_search_form(); ?>
-	       </p>
+	      <img src="https://s3.amazonaws.com/SorenTech/media/404.png" class="404-logo" />
 	  </div><!-- 404-logo -->
-     </div><!-- 404-logo-row -->
+     </div><!-- logo-row -->
+     <div class="row search-row">
+	  <div class="col-xs-6 col-xs-offset-3 col-md-4 col-md-offset-4 search-form">
+	       <?php get_search_form(); ?>
+	  </div><!-- search-form -->
+     </div><!-- search-row -->
      <div class="row 404-text-row">
      	  <div class="col-sm-4 col-xs-offset-2 404-explanation" style="color: white">
 	       <h1>Oops!</h1>
